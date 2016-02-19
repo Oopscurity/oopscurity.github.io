de54b6dc67f39fed238e6f21a4701439ba791a78
Oopscurity <oopscurity@mail.ru>
2016-02-19 14:56:30 +0400

    Changed style of the sidebar

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -5,6 +5,7 @@ module.exports = {
     head : [
         { elem : 'meta', attrs : { name : 'description', content : '' } },
         { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
+        { elem: 'css', url: 'https://fonts.googleapis.com/css?family=Raleway:300,400' },
         { elem : 'css', url : 'index.min.css' }
     ],
     scripts: [
@@ -12,68 +13,91 @@ module.exports = {
     ],
     content : [
         {
-            block: 'header',
+            block: 'sidebar',
             content: [
                 {
-                    elem: 'section',
+                    block: 'header',
                     content: [
                         {
-                            block: 'about',
+                            elem: 'section',
                             content: [
                                 {
-                                    elem: 'main',
+                                    block: 'about',
                                     content: [
                                         {
+                                            elem: 'main',
+                                            content: [
+                                                {
+                                                    block: 'text',
+                                                    mods: { type: 'title', ctx: 'page' },
+                                                    mix: { block: 'about', elem: 'name' },
+                                                    content: 'Artyom Kravchenko'
+                                                },
+                                                {
+                                                    block: 'text',
+                                                    mods: { type: 'description' },
+                                                    mix: { block: 'about', elem: 'profession' },
+                                                    content: 'Front End Developer'
+                                                }
+                                            ]
+                                        },
+                                        {
                                             block: 'text',
-                                            mods: { type: 'title' },
-                                            content: 'Artyom Kravchenko'
+                                            mods: { type: 'title', ctx: 'page' },
+                                            mix: { block: 'about', elem: 'promo' },
+                                            content: 'True quality in the website development'
                                         },
                                         {
                                             block: 'text',
                                             mods: { type: 'description' },
-                                            content: 'Front End Developer'
+                                            mix: { block: 'about', elem: 'skills' },
+                                            content: [
+                                                'HTML', 'CSS', 'JavaScript', 'BEM'
+                                            ].join(' / ')
                                         }
                                     ]
-                                },
+                                }
+                            ]
+                        },
+                        {
+                            elem: 'section',
+                            content: [
                                 {
-                                    block: 'text',
-                                    mods: { type: 'title' },
-                                    mix: { block: 'about', elem: 'promo' },
-                                    content: 'True quality in the website development'
-                                },
-                                {
-                                    block: 'text',
-                                    mods: { block: 'description' },
-                                    mix: { block: 'about', elem: 'skills' },
+                                    block: 'social',
                                     content: [
-                                        'HTML', 'CSS', 'JavaScript', 'BEM'
-                                    ].join(' / ')
+                                        {},
+                                        {}
+                                    ].map(function(item) {
+                                        return [
+                                            {
+                                                elem: 'item',
+                                                content: [
+                                                    {
+                                                        block: 'link',
+                                                        url: item.url
+                                                    }
+                                                ]
+                                            }
+                                        ];
+                                    })
                                 }
                             ]
                         }
                     ]
                 },
                 {
-                    elem: 'section',
+                    block: 'footer',
                     content: [
                         {
-                            block: 'social',
+                            elem: 'section',
                             content: [
-                                {},
-                                {}
-                            ].map(function(item) {
-                                return [
-                                    {
-                                        elem: 'item',
-                                        content: [
-                                            {
-                                                block: 'link',
-                                                url: item.url
-                                            }
-                                        ]
-                                    }
-                                ];
-                            })
+                                {
+                                    block: 'text',
+                                    mods: { type: 'description' },
+                                    mix: { block: 'copyright' },
+                                    content: '2016 &copy; Artyom Kravchenko'
+                                }
+                            ]
                         }
                     ]
                 }
@@ -83,12 +107,6 @@ module.exports = {
             block: 'content',
             content: [
 
-            ]
-        },
-        {
-            block: 'footer',
-            content: [
-                
             ]
         }
     ]
