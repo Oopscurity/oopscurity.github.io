0b217becf479088a951902f5e4213755da3f7379
Oopscurity <oopscurity@mail.ru>
2016-02-21 06:49:06 +0400

    Added Yandex.Metrika counter

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -117,6 +117,22 @@ module.exports = {
                     ]
                 }
             ]
-        }
+        },
+        '<!-- Yandex.Metrika counter -->',
+        {
+            tag: 'script',
+            content: [
+                '// Yandex.Metrika counter\n',
+                '(function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter33371013 = new Ya.Metrika({ id:33371013, clickmap:true, trackLinks:true, accurateTrackBounce:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");',
+                '// /Yandex.Metrika counter'
+            ]
+        },
+        {
+            tag: 'noscript',
+            content: [
+                '<div><img src="https://mc.yandex.ru/watch/33371013" style="position:absolute; left:-9999px;" alt="" /></div>'
+            ]
+        },
+        '<!-- /Yandex.Metrika counter -->'
     ]
 };
